9ecbc685a89f53750f1d1b67ee3dea5916380416
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-14 18:39:29 +0000

    add starter files

---
+++ src/main/java/com/raxrot/sbraxrotschool/SbRaxRotSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbraxrotschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRaxRotSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRaxRotSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/sbraxrotschool/SbRaxRotSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbraxrotschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRaxRotSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
